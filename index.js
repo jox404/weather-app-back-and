40ba8a521249b86ff055bf78c905cc774fba0f79
jox404 <joaovitorribeiroc1@gmail.com>
2023-01-03 23:28:25 -0300

    deploy vercel

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-/* import routes from "./src/routes/index.js";
-import db from "./src/db.js"; */
+import routes from "./src/routes/index.js";
+import db from "./src/db.js";
 
 const app = express();
 
-/* app.use(express.json());
+app.use(express.json());
 app.use(routes.routes);
 
-db.sync(() => console.log(`Banco de dados conectado:${process.env.DB_NAME}`)); */
+db.sync(() => console.log(`Banco de dados conectado:${process.env.DB_NAME}`));
 
-app.use("/", (req, res) => {
+/* app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
-});
+}); */
 
 const PORT = 8080;
 
